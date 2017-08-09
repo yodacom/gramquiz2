3e80a6f0861de8ac6f96bb1f3187be7c137cc404
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-08-09 09:51:26 -0400

    Break up client side javascript into separate files

---
+++ public/js/email.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('#send').click(function (e) {
+        e.preventDefault();
+        $(this).addClass('animated fadeOutRight');
+        $('.box form').addClass('animated fadeOut');
+
+        $(this, '.box form').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd  oanimationend animationend', function () {
+            $(this).hide();
+            $('.box form').hide();
+            $('.thanks').show().addClass('animated zoomIn');
+            $('.box').height('auto');
+        });
+    });
+});
---
+++ public/js/quiz.js
@@ -0,0 +1,141 @@
+/**
+ * The main client side JS file
+ */
+var quiz;
+
+function displayWordGroup(keywords) {
+    if (keywords.length === 0) {
+        $('.wordlist').append('<h3> End of list Press the Analysis button to see your profile </h4>');
+        $('#btnNext').off();
+        $('#btnNext').click(performAnalysis);
+        $('#btnNext').text('Analysis');
+    } else {
+        currentWords = [];
+        $('.wordlist').empty();
+        for (let i = 0; i < Math.min(5, keywords.length); i++) {
+            const rnd = Math.floor(Math.random() * keywords.length);
+            const word = keywords[rnd];
+            currentWords.push(word);
+            keywords.splice(rnd, 1);
+            $('.wordlist').append(`<span class='word' data-id='${word._id}'>${word.word}</span>`);
+        }
+
+        console.log(currentWords);
+    }
+}
+
+
+function moveWord(elem) {
+    const word = $(this).text();
+    const word_id = $(this).data("id");
+    $(this).detach().prependTo('#answerListBox .bestWords');
+    const index = currentWords.findIndex((w) => w.word === word);
+    //person.addWord(currentWords[index]);
+    $.ajax({
+        method: 'PUT',
+        url: '/quiz/bestword',
+        data: {
+            word: word,
+            id: word_id,
+            quiz: quiz._id
+        }
+    })
+        .done(function (res) {
+            console.log(res.message);
+        });
+    currentWords.splice(index, 1);
+}
+
+function removeWord(elem) {
+    const word = $(this).text();
+    const word_id = $(this).data("id");
+    // bestWords = [];
+    $(this).detach().prependTo('#chooseWordBox .wordlist');
+    // const index = bestWords.findIndex((w)=> w.word = word);
+    //person.removeWord(word);
+    $.ajax({
+        method: 'DELETE',
+        url: '/quiz/bestword',
+        data: {
+            word: word,
+            id: word_id,
+            quiz: quiz._id
+        }
+    })
+        .done(function (res) {
+            console.log(res.message);
+        });
+
+    // currentWords.push(word);
+
+}
+
+function performAnalysis() {
+    //person.analysis();
+    //personalityReport();
+    $.ajax({
+        method: 'GET',
+        url: '/quiz/analysis',
+        data: {
+            quiz: quiz._id
+        }
+    })
+        .done(function (res) {
+            console.log(res);
+            personalityReport(res);
+        });
+}
+
+function personalityReport(report) {
+    // hide quiz boxes to show report
+    $('.contentBox').hide();
+    $('.headerBox').hide();
+    $('.contentBoxDescription').hide();
+    $('.actions').hide();
+
+    $('.personalityReport').append(`<h3>${report.center}</h3>`);
+    // $('.personalityReport').append(`<h3>${report.primary}</h3>`);
+
+    // $('.personalityReport').append(`<h3>${person.getSecondary()}</h3>`);
+    $('.personalityReport').append(`<p>${report.description}</p>`);
+
+    $('.reports').show();
+}
+
+function refreshPage() {
+    location.reload();
+}
+
+
+$(document).ready(function () {
+
+    var keywords;
+
+    //load up the array of words
+    $.ajax({
+        url: '/quiz/word'
+    })
+        .done(function (data) {
+            keywords = data;
+            displayWordGroup(keywords);
+        });
+
+    $.ajax({
+        method: 'POST',
+        url: '/quiz'
+    }).done(function (data) {
+        console.log(data);
+        quiz = data;
+    });
+
+    $('#chooseWordBox').on('click', '.word', moveWord);
+    $('#answerListBox').on('click', '.word', removeWord);
+    $('#btnNext').click((e) => {
+        e.preventDefault();
+        displayWordGroup(keywords);
+    });
+
+    $('#ref_btn').click(refreshPage);
+
+
+});
